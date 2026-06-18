8c444f71be127021e18bcd22565befe04bfdc3a3
jrleolopez <jrleolopez@outlook.com>
2026-06-18 16:11:28 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+connectDB();
 
 // Importar rutas
 const authRoutes = require("./routes/authRoutes");
