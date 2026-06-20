f72472f19eac342cbb7bb8ad68264effaecf6934
jrleolopez <jrleolopez@outlook.com>
2026-06-19 21:10:41 -0500

    Add files via upload

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,16 +9,14 @@ const protect = async (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
   try {
-    // Verificar token
     const decoded = jwt.verify(token, process.env.SECRET);
 
-    // Buscar usuario en la BD y adjuntar al request
     const user = await User.findById(decoded.id).select("-password");
     if (!user) {
       return res.status(401).json({ error: "Usuario no encontrado" });
     }
 
-    req.user = user; // ahora tienes { _id, username, role, bio, avatar }
+    req.user = user;
     next();
   } catch (error) {
     res.status(401).json({ error: "Token inválido o expirado" });
