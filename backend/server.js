e98eb4748e3b79bae578f28afcf882ec042e4a1f
jrleolopez <jrleolopez@outlook.com>
2026-08-31 05:59:27 -0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,22 @@ const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 
 const app = express();
-
 const path = require("path");
+
+
 app.use(express.static(path.join(__dirname, "../frontend")));
 
+
 app.use(cors());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api", commentRoutes);
-app.use("/uploads", express.static("uploads"));
 
 connectDB();
 
@@ -26,3 +31,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+console.log("Cloudinary:", process.env.CLOUD_NAME);
+
