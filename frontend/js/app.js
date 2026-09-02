021454b0c68bd2d2d9aaabb01898172f8a77ac32
jrleolopez <jrleolopez@outlook.com>
2026-09-02 10:56:41 -0500

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -561,8 +561,8 @@ async function loadUsers() {
               <td>${user.role}</td>
               <td>
                 ${user.role === "admin"
-                  ? `<button class="btn-form" onclick="updateUserRole('${user._id}', 'user')">Revertir a Usuario</button>`
-                  : `<button class="btn-form" onclick="updateUserRole('${user._id}', 'admin')">Administrador</button>`}
+                  ? `<button class="btn-user" onclick="updateUserRole('${user._id}', 'user')">Revertir a Usuario</button>`
+                  : `<button class="btn-user" onclick="updateUserRole('${user._id}', 'admin')">Administrador</button>`}
                 <button class="btn-delete" onclick="deleteUser('${user._id}')">Eliminar</button>
               </td>
             </tr>
